c5f17dd511f95b9845671a959eac52c4ce9ee259
ofersarid <ofersarid@gmail.com>
2021-07-13 12:52:39 +0300

    wip

--- src/GA.js
+++ src/GA.js
@@ -1,12 +1,19 @@
 import ReactGA from 'react-ga';
 
-function init(trackingId, userId) {
-  ReactGA.initialize(trackingId, {
-    debug: true,
-    gaOptions: {
-      userId
-    }
-  });
+function init(trackingId, userId, options = {}) {
+  const myStorage = window.localStorage;
+  const uid = myStorage.getItem('userId');
+  if (uid !== userId) {
+    myStorage.setItem('userId', userId);
+  }
+  if (process.env.NODE_ENV === 'production' || options.devMode) {
+    ReactGA.initialize(trackingId, {
+      debug: true,
+      gaOptions: {
+        userId: myStorage.getItem('userId')
+      }
+    });
+  }
 }
 
 function pageView(label) {
